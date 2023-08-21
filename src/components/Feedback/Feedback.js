6c48bef067fd631214ddb97d35fe446849bf7eb6
VladislavSemenets <124085061+VladislavSemenets@users.noreply.github.com>
2023-08-21 15:53:29 +0300

    finished feedback with one issue

--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -1,21 +1,49 @@
 import React from 'react';
-import FeedbackStyled from "./Feedback.styled";
+import Statistics from './Statistics';
+import FeedbackOptions from './FeedbackOptions';
 
 class Feedback extends React.Component {
-    render() {
-        return (
-            <div>
-                <h2>Please leave feedback</h2>
-                <button type="button" onClick={}>Good</button>
-                <button type="button" onClick={}>Neutral</button>
-                <button type="button" onClick={}>Bad</button>
-                <h3>Statistics</h3>
-<span>Good: 0</span>
-<span>Neutral: 0</span>
-<span>Bad: 0</span>
-            </div>
-        )
-    }
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  };
+
+  handleClick = type => {
+    this.setState(prevState => ({
+      [type]: prevState[type] + 1
+    }));
+  };
+
+  render() {
+    const { good, neutral, bad, feedbackReceived } = this.state;
+    const total = good + neutral + bad;
+    const positivePercentage = total === 0 ? 0 : Math.round((good / total) * 100);
+  
+    return (
+      <div>
+        <h2>Please leave feedback</h2>
+        <FeedbackOptions
+          options={['good', 'neutral', 'bad']}
+          onLeaveFeedback={this.handleClick}
+        />
+        {feedbackReceived ? (
+          <div>
+            <h3>Statistics</h3>
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              positivePercentage={positivePercentage}
+            />
+          </div>
+        ) : (
+          <p>There is no feedback</p>
+        )}
+      </div>
+    );
+  }
 }
 
 export default Feedback;
